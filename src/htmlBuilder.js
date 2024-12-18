5c3b454b94c78d82502bd28209b5e086724133c7
leesound <93629022+leesound@users.noreply.github.com>
2024-12-18 21:03:07 +0800

    汉化.js

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -69,7 +69,7 @@ const generateGithubLink = () => `
 
 const generateCardHeader = () => `
 <div class="card-header text-center">
-<h1 class="display-4 mb-0">Sublink Worker</h1>
+<h1 class="display-4 mb-0">订阅转换</h1>
 </div>
 `;
 
